59b5a7bc6a8ec9084fedf59260f3afc13556aad3
Ivalina Nenova <nenovaivalina@gmail.com>
2023-01-24 13:28:09 +0200

    Removed unnecassary files

--- app.js
+++
@@ -1,9 +0,0 @@
-import page from './node_modules/page/page.mjs'
-import {displayHome} from './views/home.js'
-import { displayStudies } from './views/my-studies.js';
-import { displayPast } from './views/past.js';
-
-page('/', displayHome);
-page('/studies', displayStudies);
-page('/past', displayPast);
-page.start();
